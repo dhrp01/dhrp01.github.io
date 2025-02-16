79dc6534d215209ee579b25ee7fba4b5b5d42de6
Dhrumeen Patel <dhrumeen.dp@gmail.com>
2025-02-15 20:14:58 -0500

    Initiale push

---
+++ script.js
@@ -0,0 +1,55 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
+
+const roles=['Software Developer', 'Machine Learning Engineer', 'Linux Enthusiast']
+let currentIndex = 0;
+const role_text = document.getElementById('role')
+let currentRoleIndex = 0;
+let typingDirection = 1;
+
+function changeRole() {
+    // DIRECT CHANGE
+    // document.getElementById('role').innerText = roles[currentIndex];
+    // currentIndex = (currentIndex + 1) % roles.length
+    
+    // PHASE IN AND PHASE OUT
+    // const role_text = document.getElementById('role');
+    // role_text.style.opacity = 0;
+    // setTimeout(() => {
+    // role_text.innerText = `I am ${roles[currentIndex]}`;
+    // role_text.style.opacity = 1;
+    // currentIndex = (currentIndex + 1) % roles.length
+    // }, 1000
+    // )
+
+    // DELETING AND TYPING
+    const role = roles[currentIndex];
+    const shortenedRole = role.substring(0, currentRoleIndex);
+    role_text.innerText = shortenedRole;
+    role_text.style.height = `${role_text.scrollHeight}px`
+    
+    if (typingDirection === 1){
+        currentRoleIndex++;
+        if (currentRoleIndex > role.length) {
+            typingDirection = -1;
+            currentRoleIndex = role.length;
+            setTimeout(() => {
+                // Start Deleting after some delay
+                typingDirection = -1;
+            }, 500
+            )
+        }
+    } else {
+        currentRoleIndex--;
+        if (currentRoleIndex === 0) {
+            typingDirection = 1;
+            currentIndex = (currentIndex + 1) % roles.length
+        }
+    }
+}
+
+setInterval(changeRole, 60)
